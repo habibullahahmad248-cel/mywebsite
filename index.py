42cb06333877ab62cbb5a39a82d7d06446d3708b
habib <habibullahahmad248@.com>
2026-01-29 09:29:59 +0000

    third website upload

---
+++ index.py
@@ -0,0 +1,33 @@
+print("Assalmu ailaikum habibullah")
+suna"habibullah"
+shekara= 20
+print("suna")
+print("shekara")
+
+suna ="habibullah"
+kasa="Nigeria"
+suna=input("habibullah")
+print("sannu;habibullah")
+suna=input("sunanka;habibullah")
+print("shekarunka;20")
+shekara=int(input("shekarunka;nawa?:"))
+print("shekara + 5")
+age = int(input("Shekara: "))
+print(age)
+ age=int(input("shekara:"))
+ print(age)
+  age=int(input("shekara:"))
+  print(age)
+  age=int(input("shekara:"))
+  print(age)
+  age=int(input(shekara:)
+  print(age)
+  age=int(input("shekara:")))
+  print(age)
+  age=int(input("shekara:20"))
+  print(20)
+  age=int(input("shekara:20"))
+  print(age)
+  age=int(input("shekara:30"))
+  print(age)
+   
